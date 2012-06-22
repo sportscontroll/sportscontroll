b95a441aad4ec3b3a1a86948d5d8c1a9767d5c0c
schoko@grace <schoko@grace>
2012-06-22 10:07:02 +0200

    ADD     first Class
    FEATURE
        add java stuff to gitignore

---
+++ src/de/dhbw/sportcontroll/main/SportControll.java
@@ -0,0 +1,14 @@
+package de.dhbw.sportcontroll.main;
+
+public class SportControll {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
