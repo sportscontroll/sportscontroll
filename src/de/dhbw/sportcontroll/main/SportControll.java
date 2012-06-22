83e0392aecef2896c0525da01c592f0c5bd5d635
schoko@grace <schoko@grace>
2012-06-22 10:31:31 +0200

    REWORK 	test

--- src/de/dhbw/sportcontroll/main/SportControll.java
+++ src/de/dhbw/sportcontroll/main/SportControll.java
@@ -7,7 +7,7 @@ public class SportControll {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World und noch mal");
 
 	}
 
