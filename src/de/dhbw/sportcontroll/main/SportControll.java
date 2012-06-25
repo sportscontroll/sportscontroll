52e57984bbb03aade19efe71c7e0df944fcba5fd
schoko@grace <schoko@grace>
2012-06-25 11:51:02 +0200

    kleiner test

--- src/de/dhbw/sportcontroll/main/SportControll.java
+++ src/de/dhbw/sportcontroll/main/SportControll.java
@@ -8,6 +8,7 @@ public class SportControll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World und noch mal");
+		System.out.println("Hello World und noch mal");
 
 	}
 
